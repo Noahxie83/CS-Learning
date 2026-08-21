41e8aec03165ed2f8006a1978fde72daa9fa0452
Noahxie83 <146695987+Noahxie83@users.noreply.github.com>
2026-08-22 01:44:49 +0800

    Add files via upload

---
+++ Python MIT6.100L/Lecture 1 Introduction/mit6_100l-f22_ex01.py
@@ -0,0 +1,8 @@
+#Assume 3 variables are already defined for you: a, b, and c. 
+#Create a variable called total that adds a and b then multiplies the result by c. 
+#Include a last line in your code to print the value: print(total)
+a=3
+b=4
+c=5
+total=(a+b)*c
+print(total)
