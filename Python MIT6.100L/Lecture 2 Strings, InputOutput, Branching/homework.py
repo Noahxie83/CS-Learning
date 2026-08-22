d832ffd57be800fabf167a96609730142f32d8a1
Noahxie83 <146695987+Noahxie83@users.noreply.github.com>
2026-08-22 21:08:46 +0800

    Add files via upload

---
+++ Python MIT6.100L/Lecture 2 Strings, InputOutput, Branching/homework.py
@@ -0,0 +1,60 @@
+# Practice 1: What is the value of s1 and s2?
+s1 = "a" + "b"
+
+d = "hi"
+e = " ana"
+s2 = d + 2*e
+
+s1 = ab
+s2 = hi ana ana
+
+# Practice 2: What are the substrings of s?
+s = "ABC d3f ghi"
+s[0:3:1]       
+s[0:4]       
+s[8:len(s):3] 
+s[2::-1]      
+ABC
+ABC 
+g
+CBA
+
+# Practice 3: What does this print?
+# Note that a += b is the same as a = a + b
+# answer = ''
+# x = 11
+# # try with y = 2 and y = 12
+# y = 2
+# if len(str(x)) == len(str(y)):
+#     if y != 0 and x%2 == 1:
+#         answer = answer + "x / y is " + str(x/y)
+# elif x < y:
+#     answer += "\nx is smaller"  # \n inserts a newline character in the string
+# else:
+#     answer += "\ny is smaller"
+# print(answer)
+y=2: y is smaller
+y=12: x / y is 0.9166666666666666
+
+
+# Practice 4: Uncomment the code below and:
+# What does it print when a = 6 and b = "6"
+# What does it print when a = "1" and b = 1
+# What does it print when a = 3 and b = 3
+# What does it print when a = "1" and b = "1"
+
+# if ( a == int(b) ):
+#     print("int conversion")
+# if ( a == int(b) ) and ( str(a) == b ):
+#     print("int and str conversion")
+# else: 
+#     print("interesting")
+int conversion
+int and str conversion
+
+interesting
+
+int conversion
+interesting
+
+interesting
