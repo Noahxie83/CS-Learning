3e21f2aa57831cb483c8432aeca4b78b44b1da53
Noahxie83 <146695987+Noahxie83@users.noreply.github.com>
2026-08-28 11:17:22 +0800

    Add files via upload

---
+++ Python MIT6.100L/Lecture 1 Introduction/mit6_100l_f22_ps0/ps0.py
@@ -0,0 +1,11 @@
+# Write a program that does the following in order:
+# At the top of your file and type: import numpy
+# Now write a line that sets a variable named x to 5.
+# Now write a line that sets a variable named y to 8.
+# Add variables x and y, and save the result to a variable named z.
+# Now save the result of this command: numpy.log2(z) to a variable named a.
+import numpy
+x=5
+y=8
+z=x+y
+a=numpy.log2(z)
