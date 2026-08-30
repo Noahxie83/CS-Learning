966bd96ada5c44710886b884288d351c2f9c5059
Noahxie83 <146695987+Noahxie83@users.noreply.github.com>
2026-08-30 15:00:56 +0800

    Add files via upload

---
+++ Python MIT6.100L/Lecture 12 List Comprehension, Functions as Objects, Testing, Debugging/homework.py
@@ -0,0 +1,8 @@
+## Write a list comprehension expression that uses a list named L.
+# It makes a new list whose elements are the middle 
+# character of strings whose length is 3. 
+
+# If L = ['abc', 'm', 'p', 'xyz', '123', 57]
+# It makes ['b', 'y', '2']
+L = ['abc', 'm', 'p', 'xyz', '123', 57]
+print([e[1] for e in L if type(e)==str and len(e)==3])
